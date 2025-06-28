78311ff35199b3d9b6b620eedf1da2d2dcbbac07
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2025-06-28 20:07:28 +0530

    Update render_template.py

--- web/utils/render_template.py
+++ web/utils/render_template.py
@@ -7,9 +7,10 @@ from web.server.exceptions import InvalidHash
 import urllib.parse
 import logging
 import aiohttp
-from pathlib import Path
-from Template import jisshu_template
 
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 async def render_page(id, secure_hash, src=None):
     file = await Webavbot.get_messages(int(BIN_CHANNEL), int(id))
@@ -37,14 +38,6 @@ async def render_page(id, secure_hash, src=None):
     with open(template_file) as f:
         template = jinja2.Template(f.read())
 
-    BASE_DIR = Path(__file__).resolve().parent.parent  # Goes from /util → /Deendayal_botz
-    template_path = BASE_DIR / "template"
-
-    # ✅ Load Jinja2 environment
-    template_loader = jinja2.FileSystemLoader(searchpath=str(template_path))
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template(Path(template_file).name)
-
     file_name = file_data.file_name.replace("_", " ")
 
     return template.render(
@@ -52,8 +45,8 @@ async def render_page(id, secure_hash, src=None):
         file_url=src,
         file_size=file_size,
         file_unique_id=file_data.unique_id,
-        template_ne=jisshu_template.JISSHU_NAME,
-        jisshu_disclaimer=jisshu_template.JISSHU_DISCLAIMER,
-        jisshu_report_link=jisshu_template.JISSHU_REPORT_LINK,
-        jisshu_colours=jisshu_template.JISSHU_COLOURS
     )
+
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
